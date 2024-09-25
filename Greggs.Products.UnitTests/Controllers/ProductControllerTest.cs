f4d8f550273569acfd926bac29fa04ce109e156d
buglass <38510232+buglass@users.noreply.github.com>
2024-09-25 21:46:52 +0100

    provide data source to product controller

--- Greggs.Products.UnitTests/Controllers/ProductControllerTest.cs
+++ Greggs.Products.UnitTests/Controllers/ProductControllerTest.cs
@@ -1,7 +1,10 @@
 using System;
-using System.Linq;
+using System.Collections.Generic;
 using Greggs.Products.Api.Controllers;
+using Greggs.Products.Api.DataAccess;
+using Greggs.Products.Api.Models;
 using Microsoft.Extensions.Logging;
+using Moq;
 using Xunit;
 
 namespace Greggs.Products.UnitTests;
@@ -9,79 +12,150 @@ namespace Greggs.Products.UnitTests;
 public class ProductControllerTest
 {
     [Fact]
-    public void GetWithNoPagingReturnsFiveItems()
+    public void GetWithNoPagingReturnsFirstFiveItems()
     {
-        Assert.Equal(
-            expected:
-                5,
-            actual:
-                new ProductController(new Moq.Mock<ILogger<ProductController>>().Object)
-                .Get()
-                .Count()
-        );
-	}
-
-	[Fact]
-	public void GetWithPageLimitReturnsRequestedItemCount()
-	{
-		const int pageSize = 2;
+		IEnumerable<Product> products = new[]
+		{
+			new Product{Name = "One"},
+			new Product{Name = "Two"},
+			new Product{Name = "Three"},
+			new Product{Name = "Four"},
+			new Product{Name = "Five"},
+			new Product{Name = "Six"},
+		};
+		var dataAccess = new Mock<IDataAccess<Product>>();
+		dataAccess.Setup(da => da.List(It.IsAny<int>(), It.IsAny<int>())).Returns(products);
 
 		Assert.Equal(
 			expected:
-				pageSize,
+				new[]
+				{
+					new Product{Name = "One"},
+					new Product{Name = "Two"},
+					new Product{Name = "Three"},
+					new Product{Name = "Four"},
+					new Product{Name = "Five"},
+					new Product{Name = "Six"},
+				},
 			actual:
-				new ProductController(new Moq.Mock<ILogger<ProductController>>().Object)
-				.Get(pageStart: 0, pageSize: pageSize)
-				.Count()
+				new ProductController(new Mock<ILogger<ProductController>>().Object, dataAccess.Object)
+				.Get()
 		);
 	}
 
 	[Fact]
-	public void GetPageWithPageLimitReturnsRequestedItemCount()
+	public void GetWithPageLimitReturnsRequestedItems()
 	{
-		const int pageSize = 2;
+		IEnumerable<Product> products = new[]
+		{
+			new Product{Name = "One"},
+			new Product{Name = "Two"},
+			new Product{Name = "Three"},
+			new Product{Name = "Four"},
+			new Product{Name = "Five"},
+			new Product{Name = "Six"},
+		};
+		var dataAccess = new Mock<IDataAccess<Product>>();
+		dataAccess.Setup(da => da.List(It.IsAny<int>(), It.IsAny<int>())).Returns(products);
 
 		Assert.Equal(
 			expected:
-				pageSize,
+				new[]
+				{
+					new Product{Name = "One"},
+					new Product{Name = "Two"},
+				},
 			actual:
-				new ProductController(new Moq.Mock<ILogger<ProductController>>().Object)
-				.Get(pageStart: 1, pageSize: pageSize)
-				.Count()
+				new ProductController(new Mock<ILogger<ProductController>>().Object, dataAccess.Object)
+				.Get(pageStart: 0, pageSize: 2)
 		);
 	}
 
 	[Fact]
-	public void GetWithOversizedPageReturnsMaximumItemCount()
+	public void GetPageWithPageLimitReturnsRequestedItems()
 	{
+		IEnumerable<Product> products = new[]
+		{
+			new Product{Name = "One"},
+			new Product{Name = "Two"},
+			new Product{Name = "Three"},
+			new Product{Name = "Four"},
+			new Product{Name = "Five"},
+			new Product{Name = "Six"},
+		};
+		var dataAccess = new Mock<IDataAccess<Product>>();
+		dataAccess.Setup(da => da.List(It.IsAny<int>(), It.IsAny<int>())).Returns(products);
+
 		Assert.Equal(
 			expected:
-				5,
+				new[]
+				{
+					new Product { Name = "Three" },
+					new Product { Name = "Four" },
+				},
 			actual:
-				new ProductController(new Moq.Mock<ILogger<ProductController>>().Object)
-				.Get(pageStart: 0, pageSize: 42)
-				.Count()
+				new ProductController(new Mock<ILogger<ProductController>>().Object, dataAccess.Object)
+				.Get(pageStart: 1, pageSize: 2)
+		); ;
+	}
+
+	[Fact]
+	public void GetWithOversizedPageReturnsAllItems()
+	{
+		IEnumerable<Product> products = new[]
+		{
+			new Product{Name = "One"},
+			new Product{Name = "Two"},
+			new Product{Name = "Three"},
+			new Product{Name = "Four"},
+			new Product{Name = "Five"},
+			new Product{Name = "Six"},
+		};
+		var dataAccess = new Mock<IDataAccess<Product>>();
+		dataAccess.Setup(da => da.List(It.IsAny<int>(), It.IsAny<int>())).Returns(products);
+
+		Assert.Equal(
+			expected:
+				products,
+			actual:
+				new ProductController(new Mock<ILogger<ProductController>>().Object, dataAccess.Object)
+				.Get(pageStart: 0, pageSize: 7)
 		);
 	}
 
 	[Fact]
 	public void GetWithUnavailablePageReturnsDefaultPage()
 	{
+		IEnumerable<Product> products = new[]
+		{
+			new Product{Name = "One"},
+			new Product{Name = "Two"},
+		};
+		var dataAccess = new Mock<IDataAccess<Product>>();
+		dataAccess.Setup(da => da.List(It.IsAny<int>(), It.IsAny<int>())).Returns(products);
+
 		Assert.Equal(
 			expected:
-				5,
+				products,
 			actual:
-				new ProductController(new Moq.Mock<ILogger<ProductController>>().Object)
-				.Get(pageStart: 999, pageSize: 42)
-				.Count()
+				new ProductController(new Mock<ILogger<ProductController>>().Object, dataAccess.Object)
+				.Get(pageStart: 1, pageSize: 2)
 		);
 	}
 
 	[Fact]
 	public void GetEmptyPageReturnsNoItems()
 	{
+		IEnumerable<Product> products = new[]
+{
+			new Product{Name = "One"},
+			new Product{Name = "Two"},
+		};
+		var dataAccess = new Mock<IDataAccess<Product>>();
+		dataAccess.Setup(da => da.List(It.IsAny<int>(), It.IsAny<int>())).Returns(products);
+
 		Assert.Empty(
-			new ProductController(new Moq.Mock<ILogger<ProductController>>().Object)
+			new ProductController(new Mock<ILogger<ProductController>>().Object, dataAccess.Object)
 			.Get(pageStart: 0, pageSize: 0)
 		);
 	}
@@ -89,21 +163,42 @@ public class ProductControllerTest
 	[Fact]
 	public void GetPageBelowZeroReturnsDefaultPage()
 	{
+		IEnumerable<Product> products = new[]
+		{
+			new Product{Name = "One"},
+			new Product{Name = "Two"},
+			new Product{Name = "Three"},
+			new Product{Name = "Four"},
+			new Product{Name = "Five"},
+			new Product{Name = "Six"},
+		};
+		var dataAccess = new Mock<IDataAccess<Product>>();
+		dataAccess.Setup(da => da.List(It.IsAny<int>(), It.IsAny<int>())).Returns(products);
+
 		Assert.Equal(
 			expected:
-				5,
+				new[]
+				{
+					new Product{Name = "One"},
+					new Product{Name = "Two"},
+					new Product{Name = "Three"},
+					new Product{Name = "Four"},
+					new Product{Name = "Five"},
+				},
 			actual:
-				new ProductController(new Moq.Mock<ILogger<ProductController>>().Object)
+				new ProductController(new Mock<ILogger<ProductController>>().Object, dataAccess.Object)
 				.Get(pageStart: -1)
-				.Count()
 		);
 	}
 
 	[Fact]
 	public void GetPageSizeBelowZeroThrowsRangeException()
 	{
+		var dataAccess = new Mock<IDataAccess<Product>>();
+		dataAccess.Setup(da => da.List(It.IsAny<int>(), It.IsAny<int>())).Returns(new List<Product>());
+
 		Assert.Throws<ArgumentOutOfRangeException>(() =>
-			new ProductController(new Moq.Mock<ILogger<ProductController>>().Object)
+			new ProductController(new Mock<ILogger<ProductController>>().Object, dataAccess.Object)
 			.Get(pageStart: 0, pageSize: -1)
 		);
 	}
