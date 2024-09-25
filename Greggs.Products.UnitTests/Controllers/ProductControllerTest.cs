f0e2a29b73d7d284bfdd38b871f5df8e64debd75
buglass <38510232+buglass@users.noreply.github.com>
2024-09-25 20:51:04 +0100

    add page scenarios to product controller unit tests

--- Greggs.Products.UnitTests/Controllers/ProductControllerTest.cs
+++ Greggs.Products.UnitTests/Controllers/ProductControllerTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Greggs.Products.Api.Controllers;
 using Microsoft.Extensions.Logging;
@@ -18,5 +19,92 @@ public class ProductControllerTest
                 .Get()
                 .Count()
         );
+	}
+
+	[Fact]
+	public void GetWithPageLimitReturnsRequestedItemCount()
+	{
+		const int pageSize = 2;
+
+		Assert.Equal(
+			expected:
+				pageSize,
+			actual:
+				new ProductController(new Moq.Mock<ILogger<ProductController>>().Object)
+				.Get(pageStart: 0, pageSize: pageSize)
+				.Count()
+		);
+	}
+
+	[Fact]
+	public void GetPageWithPageLimitReturnsRequestedItemCount()
+	{
+		const int pageSize = 2;
+
+		Assert.Equal(
+			expected:
+				pageSize,
+			actual:
+				new ProductController(new Moq.Mock<ILogger<ProductController>>().Object)
+				.Get(pageStart: 1, pageSize: pageSize)
+				.Count()
+		);
+	}
+
+	[Fact]
+	public void GetWithOversizedPageReturnsMaximumItemCount()
+	{
+		Assert.Equal(
+			expected:
+				5,
+			actual:
+				new ProductController(new Moq.Mock<ILogger<ProductController>>().Object)
+				.Get(pageStart: 0, pageSize: 42)
+				.Count()
+		);
+	}
+
+	[Fact]
+	public void GetWithUnavailablePageReturnsDefaultPage()
+	{
+		Assert.Equal(
+			expected:
+				5,
+			actual:
+				new ProductController(new Moq.Mock<ILogger<ProductController>>().Object)
+				.Get(pageStart: 999, pageSize: 42)
+				.Count()
+		);
+	}
+
+	[Fact]
+	public void GetEmptyPageReturnsNoItems()
+	{
+		Assert.Empty(
+			new ProductController(new Moq.Mock<ILogger<ProductController>>().Object)
+			.Get(pageStart: 0, pageSize: 0)
+		);
+	}
+
+	[Fact]
+	public void GetPageBelowZeroReturnsDefaultPage()
+	{
+		Assert.Equal(
+			expected:
+				5,
+			actual:
+				new ProductController(new Moq.Mock<ILogger<ProductController>>().Object)
+				.Get(pageStart: -1)
+				.Count()
+		);
+	}
+
+	[Fact]
+	public void GetPageSizeBelowZeroThrowsRangeException()
+	{
+		Assert.Throws<ArgumentOutOfRangeException>(() =>
+			new ProductController(new Moq.Mock<ILogger<ProductController>>().Object)
+			.Get(pageStart: 0, pageSize: -1)
+		);
 	}
 }
