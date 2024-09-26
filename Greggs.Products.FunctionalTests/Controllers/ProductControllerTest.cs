50e5550bbbe78547273cfb68d47c23276e9184d3
buglass <38510232+buglass@users.noreply.github.com>
2024-09-26 23:58:01 +0100

    add currency conversion scenarios to functional tests

--- Greggs.Products.FunctionalTests/Controllers/ProductControllerTest.cs
+++ Greggs.Products.FunctionalTests/Controllers/ProductControllerTest.cs
@@ -1,4 +1,5 @@
-﻿using Greggs.Products.Api.Controllers;
+﻿using System.Xml.Linq;
+using Greggs.Products.Api.Controllers;
 using Greggs.Products.Api.CurrencyPrices;
 using Greggs.Products.Api.DataAccess;
 using Greggs.Products.Api.Models;
@@ -12,7 +13,7 @@ namespace Greggs.Products.FunctionalTests;
 public class ProductControllerTest
 {
 	[Fact]
-	public void GetWithNoPagingReturnsFirstFiveItems()
+	public void GetWithNoParametersReturnsDefaultPage()
 	{
 		Assert.Equivalent(
 			expected:
@@ -34,48 +35,68 @@ public class ProductControllerTest
 	}
 
 	[Fact]
-	public void GetWithPageLimitReturnsRequestedItems()
+	public void GetWithEurosCurrencyParametersReturnsEurosPrices()
 	{
 		Assert.Equivalent(
 			expected:
 				new List<Product>
 				{
-					new Product { Name = "Sausage Roll", PriceInPounds = 1m },
-					new Product { Name = "Vegan Sausage Roll", PriceInPounds = 1.1m },
+					new Product { Name = "Sausage Roll", PriceInPounds = 1.11m },
+					new Product { Name = "Vegan Sausage Roll", PriceInPounds = 1.22m },
+					new Product { Name = "Steak Bake", PriceInPounds = 1.33m },
+					new Product { Name = "Yum Yum", PriceInPounds = 0.78m },
+					new Product { Name = "Pink Jammie", PriceInPounds = 0.56m },
 				},
 			actual:
 				new ProductController(
 					new Mock<ILogger<ProductController>>().Object,
 					new ProductAccess(),
 					new CurrencyPriceConverter(new CurrencyConversionRates())
-				).Get(pageStart: 0, pageSize: 2)
+				).Get(currencyCode: "EUR")
 		);
 	}
 
 	[Fact]
+	public void GetWithPageLimitReturnsRequestedItems()
+	{
+		Assert.Equivalent(
+		expected:
+		new List<Product>
+		{
+			new Product { Name = "Sausage Roll", PriceInPounds = 1m },
+			new Product { Name = "Vegan Sausage Roll", PriceInPounds = 1.1m },
+		},
+		actual:
+		new ProductController(
+		new Mock<ILogger<ProductController>>().Object,
+		new ProductAccess(),
+		new CurrencyPriceConverter(new CurrencyConversionRates())
+		).Get(pageStart: 0, pageSize: 2)
+		);
+	}
+	[Fact]
 	public void GetPageWithPageLimitReturnsRequestedItems()
 	{
 		Assert.Equivalent(
-			expected:
-				new List<Product>
-				{
-					new Product { Name = "Steak Bake", PriceInPounds = 1.2m },
-					new Product { Name = "Yum Yum", PriceInPounds = 0.7m },
-				},
-			actual:
-				new ProductController(
-					new Mock<ILogger<ProductController>>().Object,
-					new ProductAccess(),
-					new CurrencyPriceConverter(new CurrencyConversionRates())
-				).Get(pageStart: 1, pageSize: 2)
+		expected:
+		new List<Product>
+		{
+			new Product { Name = "Steak Bake", PriceInPounds = 1.2m },
+			new Product { Name = "Yum Yum", PriceInPounds = 0.7m },
+		},
+		actual:
+		new ProductController(
+		new Mock<ILogger<ProductController>>().Object,
+		new ProductAccess(),
+		new CurrencyPriceConverter(new CurrencyConversionRates())
+		).Get(pageStart: 1, pageSize: 2)
 		); ;
 	}
-
 	[Fact]
 	public void GetPageBelowZeroReturnsDefaultPage()
 	{
 		Assert.Equivalent(
-			expected:
+		expected:
 				new List<Product>
 				{
 					new Product { Name = "Sausage Roll", PriceInPounds = 1m },
